a5881bcca03f65220a6e2aa7f4f1f5d96b3f90be
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-03-12 23:07:28 -0500

    update

--- sdc_maintenance/models/maintenance.py
+++ sdc_maintenance/models/maintenance.py
@@ -173,7 +173,7 @@ class MaintenanceEquipement(models.Model):
 
     zone_id=fields.Many2one('maintenance.zone', u'Zone')
 
-    client_id=fields.Many2one('res.partner',related='equipment_id.client_id', string="Client", store=True, readonly=True)
+    client_id=fields.Many2one('res.partner', string='Clients')
 
     model_id=fields.Many2one('maintenance.equipment.model', u'Models')
 
