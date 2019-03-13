37f0fac585f41393a06fa047e3ece72bfc442546
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-03-12 23:30:08 -0500

    Update __manifest__.py

--- sdc_maintenance/__manifest__.py
+++ sdc_maintenance/__manifest__.py
@@ -2,7 +2,7 @@
 # Author : Addi Ait-Mlouk
 {
     "name": "Soporte Técnico",
-    "version": "1.5",
+    "version": "1.6",
     "depends": ['maintenance', "mail","sale",'website'],
     "author": "Rocendo Tejada - Electronica Médica",
     'website': 'https://www.electronicamedica.com/',
