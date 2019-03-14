3010cef9456207d0cd31671cf50a6f68069a563f
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-03-13 23:53:54 -0500

    Update helpdesk.py

--- maintenance_helpdesk/models/helpdesk.py
+++ maintenance_helpdesk/models/helpdesk.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from dateutil.relativedelta import relativedelta
-from dateutil.relativedelta import *
-from dateutil import relativedelta
-
 from odoo import api, fields, models, SUPERUSER_ID, _, exceptions
+import time
+import datetime as dt 
+import time, datetime
+from datetime import date
+from datetime import datetime
+from dateutil import relativedelta
+from datetime import datetime, timedelta
+from odoo.exceptions import UserError
+from dateutil.relativedelta import *
 
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
