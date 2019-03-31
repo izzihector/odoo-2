c6d3f582a75845b223fe4fa206af2745f7373fed
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-03-30 21:34:36 -0500

    update

--- equipment/__manifest__.py
+++ equipment/__manifest__.py
@@ -38,6 +38,7 @@ Support following feature:
         'views/equipment_model_view.xml',
         'views/equipment_software_view.xml',
         'views/equipment_dicom_view.xml',
+        'views/equipment_network_view.xml',
         'views/equipment.xml',
         'data/equipment_data.xml',
         'data/stock_data.xml',
