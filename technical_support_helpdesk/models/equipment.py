8b250c93eca0b022f20b769593d590461bab4246
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-03-31 20:58:14 -0500

    update

--- technical_support_helpdesk/models/equipment.py
+++ technical_support_helpdesk/models/equipment.py
@@ -7,3 +7,4 @@ class EquipmentModality(models.Model):
     _inherit = 'equipment.modality'
 
     team_id=fields.Many2one('helpdesk.team', string='Team')
+    team_leader_id=fields.Many2one('res.users', related='team_id.leader_id', string='Team Leader', store=True, readonly=True)
