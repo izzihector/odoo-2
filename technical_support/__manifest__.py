f8c25d205b4d0cdce096536d3f01d80e47a85dd3
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-04-01 13:43:14 -0500

    Update __manifest__.py

--- technical_support/__manifest__.py
+++ technical_support/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Technical Support',
-    'version': '1.0',
+    'version': '1.1',
     'summary': 'Equipment Maintenance, Diagnostic, Preventive, Corrective, Repair and Operation',
     'description': """
 Manage Maintenance process in Odoo
