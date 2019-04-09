f24cfe74b394729f31c56d717257a38af4d65578
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-04-09 14:26:14 -0500

    update

--- technical_support_helpdesk/__manifest__.py
+++ technical_support_helpdesk/__manifest__.py
@@ -16,6 +16,7 @@ This shortcut allows you to generate a Technical Support Request.
     'depends': ['helpdesk', 'technical_support', 'equipment'],
     'data': [
         'views/technical_support_request_views.xml',
+        'views/technical_support_order_views.xml',
         'views/helpdesk_technical_support_templates.xml',
         'views/helpdesk_views.xml',
         'views/equipment_views.xml',
